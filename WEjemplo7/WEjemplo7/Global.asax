013672815f6ee4b296edc4fb772a6fe43b1800e4
rchoquere <r.choquere@hotmail.com>
2017-11-07 15:22:42 -0400

    Inicio

---
+++ WEjemplo7/WEjemplo7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEjemplo7.MvcApplication" Language="C#" %>
---
+++ WEjemplo7/WEjemplo7/Models/Entidad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEjemplo7.Models
+{
+    public class Entidad
+    {
+        public int EntidadID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
